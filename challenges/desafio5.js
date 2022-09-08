cb3840f3842df66f440406dd4bff9c3fb5a20b27
Marcos Danilo <marcosdanilo94@gmail.com>
2022-09-08 16:19:21 -0300

    feat: :sparkles: Requisito 5 ok
    Retorne o nome, as curtidas e vendidos dos lanches que tiveram quantidade de curtidas igual a 36 ou tenham a quantidade de vendas igual a 85

---
+++ challenges/desafio5.js
@@ -0,0 +1,14 @@
+db.produtos.find(
+  {
+    $or: [
+      { curtidas: { $eq: 36 } },
+      { vendidos: { $eq: 85 } },
+    ],
+  },
+  {
+    _id: 0,
+    nome: 1,
+    vendidos: 1,
+    curtidas: 1,
+  },
+);
