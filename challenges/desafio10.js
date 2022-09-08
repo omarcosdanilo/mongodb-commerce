d2481efb3975c380eb7ab6bd33992ac930e52578
Marcos Danilo <marcosdanilo94@gmail.com>
2022-09-08 16:47:07 -0300

    feat: :sparkles: Requisito 10 ok
    Retorne o nome de todos os lanches que tenham o percentual de proteínas maior ou igual a 30 e menor ou igual a 40

---
+++ challenges/desafio10.js
@@ -0,0 +1,20 @@
+db.produtos.find(
+  {
+    $and: [
+      {
+        "valoresNutricionais.3.percentual": {
+          $gte: 30,
+        },
+      },
+      {
+        "valoresNutricionais.3.percentual": {
+          $lte: 40,
+        },
+      },
+    ],
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
